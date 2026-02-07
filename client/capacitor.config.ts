47ebcb8e149e99434c5971afb5546c130035a70a
Yahya Elmami <yahyaelmami9@gmail.com>
2026-02-07 19:06:43 +0000

    Feat: Install @capacitor/http for native networking

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.yamishop.app',
   appName: 'Yamishop',
-  webDir: 'dist/client/browser'
+  webDir: 'dist/client/browser',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true,
+    },
+  },
 };
 
 export default config;
