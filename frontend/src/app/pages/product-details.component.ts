8e19a48560247d9d842173e88c9a09e6985e69e4
Yahya Elmami <yahyaelmami9@gmail.com>
2026-06-25 19:06:45 +0000

    fix: restore missing div tag in product details

--- frontend/src/app/pages/product-details.component.ts
+++ frontend/src/app/pages/product-details.component.ts
@@ -164,6 +164,7 @@ import { SeoService } from '../services/seo.service';
                   </div>
                 </div>
               </div>
+            </div>
 
           <ng-template #notFound>
             <div class="flex-grow flex flex-col items-center justify-center gap-4 text-gray-500 py-20 px-4">
